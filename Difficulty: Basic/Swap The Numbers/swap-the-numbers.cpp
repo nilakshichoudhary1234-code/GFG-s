23ab79722103f1e5c45d8b30ce1aac0db59d26f5
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-03-22 22:54:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    
+
+
+    cout << a << " " << b << endl;
+    return 0;
+}
