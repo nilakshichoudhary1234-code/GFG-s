5ae7a12828d8baaec0759ffc84bff532dfb2e92a
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-03-23 23:42:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find element at a given Index/find-element-at-a-given-index.cpp
@@ -0,0 +1,10 @@
+
+class Solution {
+  public:
+    int findElementAtIndex(int key, vector<int> &arr) {
+        return arr[key];
+        
+    }
+};
+
+//return is used where the function signature starts with the datatype i( int,string , bool)
