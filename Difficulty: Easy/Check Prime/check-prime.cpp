04e436d2c37bd7978373ebae43fe9b650717a069
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-03-24 23:47:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Prime/check-prime.cpp
@@ -0,0 +1,19 @@
+// User function Template for C++
+
+bool prime(int n) {
+
+    // Write your code here
+    if(n<=1){
+        return false;
+    }
+    if(n<=3){
+        return true;
+    }
+    for(int i=2 ;i<=sqrt(n);i++ ){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+    // returns a boolean value
+}
