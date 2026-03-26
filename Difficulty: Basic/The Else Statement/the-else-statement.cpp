3eb196c90723a5bd98d94d9d1e244c9a8e3e399e
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-03-26 09:24:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+    if(n<=100){
+        cout<<"Small"<<'\n';
+    }
+    else{
+        cout<<"Big"<<'\n';
+    }
+    
+
+    return 0;
+}
