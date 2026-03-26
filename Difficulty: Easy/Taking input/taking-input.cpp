c03cd7f3aa3f76d86a02d41745efcf83bd7100b2
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-03-26 09:18:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input/taking-input.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void inputData() {
+        int a;
+        string b;
+        cin>>a;
+        cin>>b;
+    
+        cout<<a<<" "<<b<<'\n';
+        
+        
+    }
+};
