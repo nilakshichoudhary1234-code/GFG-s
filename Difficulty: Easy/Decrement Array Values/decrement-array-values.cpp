85233247b3daa63c6d97d800b33e6d810e03bc99
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-04-10 22:50:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Decrement Array Values/decrement-array-values.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int* decrementArrayElements(int arr[], int size) {
+        for(int i = 0;i<size;i++){
+            arr[i]=arr[i]-1;
+        }
+        return arr;
+        
+    }
+};
