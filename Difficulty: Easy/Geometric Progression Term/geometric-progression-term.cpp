ba74d19171c128fc5f6ce013325ee7ba94830b83
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-04-12 22:08:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Geometric Progression Term/geometric-progression-term.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+void utility(int a, int r, int n) {
+    // Complete the code below
+    int ans = (long long )a<<(n-1);
+        // Complete the code above
+
+        // The line below prints the output. Don't change it!
+
+        cout << ans << endl;
+}
