7f4a8b3fe7d05379750bb0ea038a9041937db182
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-04-12 22:03:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/The Else Statement/the-else-statement.cpp
+++ Difficulty: Basic/The Else Statement/the-else-statement.cpp
@@ -1,10 +1,17 @@
-void utility(int number) {
-    // Check if the number is greater than 100
-    if (number > 100) {
-        cout << "Big" << endl;
-    } 
-    // Otherwise, it must be smaller than or equal to 100
-    else {
-        cout << "Number" << endl;
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+    if(n<=100){
+        cout<<"Small"<<'\n';
     }
+    else{
+        cout<<"Big"<<'\n';
+    }
+    
+
+    return 0;
 }
