303accb43d990e1fdd94342df87019ee1a52834b
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-04-13 10:53:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/While Loop/while-loop.cpp
+++ Difficulty: Basic/While Loop/while-loop.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int x;
-    cin >> x;
-    while(x>=0){
-        cout<<x<<" ";
-        x--;
+class Solution {
+  public:
+    void utility(int x) {
+        
+        for( int i=x;i>=0 ; i--){
+            cout<< i <<" ";
+        }
+      
+        
     }
-    // use while loop over x
-    // to print integers from x to 0
-
-    return 0;
-}
+};
