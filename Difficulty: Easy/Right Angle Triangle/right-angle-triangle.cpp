9334f51f9b860a72d197ec61ff1f7b85c926cf71
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-04-16 20:43:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right Angle Triangle/right-angle-triangle.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+void triangleWall(int s) {
+
+    // Write your code here
+    for(int i=1;i<=s;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+    }
+    
+}
