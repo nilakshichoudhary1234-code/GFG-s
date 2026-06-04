ba34ad45f3e8b6b71b41b89206cdf5d5c21e0ad1
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-06-05 00:08:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Multiply Matrices/multiply-matrices.cpp
+++ Difficulty: Basic/Multiply Matrices/multiply-matrices.cpp
@@ -1,11 +1,13 @@
-/*Complete the function below*/
+/* Complete the function below */
 void multiply(int A[][100], int B[][100], int C[][100], int N)
 {
     // Loop through each row of matrix A
     for (int i = 0; i < N; i++) {
+        
         // Loop through each column of matrix B
         for (int j = 0; j < N; j++) {
-            // Initialize the current cell to 0
+            
+            // Initialize the current cell in result matrix to 0
             C[i][j] = 0;
             
             // Perform the dot product of row i and column j
