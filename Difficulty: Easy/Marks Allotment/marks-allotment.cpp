bc287efd83d20dc807689ca88cac1fc8999e5837
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-06-21 23:53:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Marks Allotment/marks-allotment.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int marks(int a) {
+        if(a>0 && a<100){
+            return a;
+        }
+        else{
+            return -1;
+        }
+        
+    }
+};
