daee971d36cbdf4da2b1f8f54261dc7338d36d6a
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-09-05 21:19:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool isPalindrome(int n) {
+        long long num=abs((long long)n);
+        long long original = num;
+        long long rev = 0;
+        
+        while(num>0){
+            rev = rev *10+(num%10);
+            num/=10;
+        }
+        
+        return original==rev;
+    }
+};
