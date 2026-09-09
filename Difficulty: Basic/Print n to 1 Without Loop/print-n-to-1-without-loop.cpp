00e5645424e1ad45335910a9b97abb69745ba7ee
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-09-09 21:37:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print n to 1 Without Loop/print-n-to-1-without-loop.cpp
@@ -0,0 +1,10 @@
+class Solution {
+    public:
+      void printNos(int n) {
+          if (n == 0) {
+              return;
+          }
+          cout << n << " ";
+          printNos(n - 1);
+      }
+  };
